6acb30db8c01ba245cfda9644005ae432e897759
Christian 'Pelle' Pelster <pelle@pelle.io>
2025-12-16 21:21:47 +0100

    remove old infra code

--- terraform/storage/main.tf
+++
@@ -1,6 +0,0 @@
-resource "hcloud_volume" "storage" {
-  name     = "${var.hostname}-data"
-  size     = 64
-  format   = "ext4"
-  location = var.location
-}
--- terraform/storage/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "cloud_api_token" {}
-
-variable "hostname" {}
-
-variable "location" {
-  default = "fsn1"
-}
